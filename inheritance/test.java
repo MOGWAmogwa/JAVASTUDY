5a75ecf593e02e301fbea7298204daec1b13e6f5
MOGWAmogwa <hongbin9696@gmail.com>
2022-08-27 22:25:46 +0900

    add inheritance

---
+++ inheritance/test.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class test {
+
+	public static void main(String[] args) {
+		int[] c = new int[3];
+		for (int i = 0 ; i < c.length ; i++) {
+			c[i] = i+1;
+		}
+
+		System.out.println(c);
+	}
+
+}
